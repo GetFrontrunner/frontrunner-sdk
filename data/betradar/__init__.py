3dea687c59c558cba3ccd7efe1931ea5e4a3170a
DrunkRandomWalker <peiyun@injectiveprotocol.com>
2022-09-19 11:26:28 -0400

    minor improvements

--- data/betradar/__init__.py
+++ data/betradar/__init__.py
@@ -35,6 +35,23 @@ class BetRadarData(Data):
             return False
 
     async def recovery_odds(self, product: str, request_id: int, n: int = 3) -> None:
+        """
+        product:
+        pre
+        liveodds
+        premium_cricket
+        betpal
+        vf
+        vbl
+        vto
+        wns
+        vdr
+        vhc
+        vti
+        vci
+        vbi
+        codds
+        """
         topic = "BetRadar/recovery_odds"
         url = f"{self.url}/{product}/recovery/initiate_request?request_id={request_id}"
 
@@ -53,6 +70,39 @@ class BetRadarData(Data):
     async def recovery_all_odds(
         self, product: str, urn_type: str, type_id: int, request_id: int, n: int = 3
     ):
+        """
+        product:
+        pre
+        liveodds
+        premium_cricket
+        betpal
+        vf
+        vbl
+        vto
+        vdr
+        vhc
+        vti
+        vci
+        vbi
+        codds
+
+        urn_type:
+        sr:match
+        sr:stage
+        sr:tournament
+        sr:season
+        vf:match
+        vbl:match
+        vto:match
+        vdr:stage
+        vhc:stage
+        vti:match
+        vci:match
+        vbi:match
+        vti:tournament
+        vci:tournament
+        vbi:tournament
+        """
         topic = "BetRadar/recovery_all_odds"
         url = f"{self.url}/{product}/odds/events/{urn_type}:{type_id}/initiate_request?request_id={request_id}"
 
@@ -71,6 +121,40 @@ class BetRadarData(Data):
     async def recovery_all_statefull_messages(
         self, product: str, urn_type: str, type_id: int, request_id: int, n: int = 3
     ):
+        """
+        product:
+        pre
+        liveodds
+        premium_cricket
+        betpal
+        vf
+        vbl
+        vto
+        vdr
+        vhc
+        vti
+        vci
+        vbi
+        codds
+
+        urn_type:
+        sr:match
+        sr:stage
+        sr:tournament
+        sr:season
+        vf:match
+        vbl:match
+        vto:match
+        vdr:stage
+        vhc:stage
+        vti:match
+        vci:match
+        vbi:match
+        vti:tournament
+        vci:tournament
+        vbi:tournament
+        """
+
         topic = "BetRadar/recovery_statefull_messages"
         url = f"{self.url}/{product}/stateful_messages/events/{urn_type}:{type_id}/initiate_request?request_id={request_id}"
 
@@ -98,6 +182,22 @@ class BetRadarData(Data):
         else:
             success = False
             logging.info("failed to get booking calendar data from betradar")
+            if not success and n > 0:
+                # success = await self._retry(topic=topic, obj=Runner, url=url)
+                n -= 1
+
+    ################################################################## CustomBet ###################################################################
+    async def get_available_selection(self, event_id: int, n: int = 3):
+        topic = "BetRadar/available_selection"
+        url = f"{self.url}/custombet/sr:match:{event_id}/available_selections"
+        res = await self.session.get(url)
+        if res.status == 200:
+            data = await res.json()
+            # runner = Runner(data)
+            # self.redis.produce(topic, dumps(runner))
+        else:
+            success = False
+            logging.info("failed to get custombet data from betradar")
             if not success and n > 0:
                 # success = await self._retry(topic=topic, obj=Runner, url=url)
                 n -= 1
@@ -133,6 +233,8 @@ class BetRadarData(Data):
                 # success = await self._retry(topic=topic, obj=Runner, url=url)
                 n -= 1
 
+    ################################################################## User Info ###################################################################
+
     async def get_user_info(self, n: int = 3):
         topic = "BetRadar/user_information"
         url = f"{self.url}/users/whoami.xml"
@@ -149,6 +251,7 @@ class BetRadarData(Data):
                 # success = await self._retry(topic=topic, obj=Runner, url=url)
                 n -= 1
 
+    ################################################################## Probability ###################################################################
     async def get_probabilities(
         self,
         urn_type: str,
@@ -177,6 +280,7 @@ class BetRadarData(Data):
                 # success = await self._retry(topic=topic, obj=Runner, url=url)
                 n -= 1
 
+    ################################################################## Betting Description ###################################################################
     async def get_markets(self, include_mappings: bool = False, n: int = 3):
         topic = "BetRadar/markets"
         url = f"{self.url}/descriptions/en/markets.xml?include_mappings={str(include_mappings).lower()}"
@@ -256,37 +360,30 @@ class BetRadarData(Data):
                 # success = await self._retry(topic=topic, obj=Runner, url=url)
                 n -= 1
 
-    async def get_market_variant(
+    async def get_market_variant_(
         self,
         market_id: int,
         variant_urn: str = "sr:exact_games:bestof:5",
         include_mappings: bool = True,
+        product: Optional[str] = None,
+        is_direct: bool = True,
         n: int = 3,
     ):
-        topic = "BetRadar/variance_market"
-        url = f"{self.url}/descriptions/en/markets/{market_id}/variants/{variant_urn}?include_mappings={str(include_mappings).lower()}"
-        res = await self.session.get(url)
-        if res.status == 200:
-            data = await res.json()
-            # runner = Runner(data)
-            # self.redis.produce(topic, dumps(runner))
-        else:
-            success = False
-            logging.info("failed to get probabilities data from betradar")
-            if not success and n > 0:
-                # success = await self._retry(topic=topic, obj=Runner, url=url)
-                n -= 1
+        """
+        product:
+        pre
+        liveodds
+        wns
+        """
 
-    async def get_product_market_variant(
-        self,
-        product: str,
-        market_id: int,
-        variant_urn: str = "sr:exact_games:bestof:5",
-        n: int = 3,
-    ):
-        topic = "BetRadar/product_variance_market"
-        url = f"{self.url}/{product}/descriptions/en/markets/{market_id}/variants/{variant_urn}"
+        topic = "BetRadar/variant_market"
+        if is_direct:
+            url = f"{self.url}/{product}/descriptions/en/markets/{market_id}/variants/{variant_urn}"
+        else:
+            url = f"{self.url}/descriptions/en/markets/{market_id}/variants/{variant_urn}?include_mappings={str(include_mappings).lower()}"
+
         res = await self.session.get(url)
+
         if res.status == 200:
             data = await res.json()
             # runner = Runner(data)
@@ -317,6 +414,8 @@ class BetRadarData(Data):
         topic = "BetRadar/all_producers"
         url = f"{self.url}/descriptions/producers.xml"
 
+    ########################################################  Entity Description  ####################################################################
+
     async def get_player_profile(self, player_id: int, n: int = 3):
         topic = "BetRadar/player_profile"
         url = f"{self.url}/sports/en/players/sr:player:{player_id}/profile.xml"
@@ -333,6 +432,11 @@ class BetRadarData(Data):
                 n -= 1
 
     async def get_competitor(self, urn_type: str, competitior_id: int, n: int = 3):
+        """
+        urn_type:
+        competitor
+        simple_team
+        """
         topic = "BetRadar/competitor"
         url = f"{self.url}/sports/en/competitors/sr:{urn_type}:{competitior_id}/profile.xml"
         res = await self.session.get(url)
@@ -362,7 +466,32 @@ class BetRadarData(Data):
                 # success = await self._retry(topic=topic, obj=Runner, url=url)
                 n -= 1
 
+    ########################################################  Sport Event Info  ####################################################################
     async def get_sport_event_summary(self, urn_type: str, event_id: int, n: int = 3):
+        """
+        sr:match
+        sr:stage
+        sr:season
+        sr:tournament
+        sr:simple_tournament
+        vf:match
+        vf:season
+        vf:tournament
+        vbl:match
+        vbl:season
+        vbl:tournament
+        vto:season
+        vto:match
+        vto:tournament
+        vdr:stage
+        vhc:stage
+        vti:match
+        vti:tournament
+        vci:match
+        vci:tournament
+        vbi:match
+        vbi:tournament
+        """
         topic = "BetRadar/event_summary"
         url = f"{self.url}/sports/en/sport_events/{urn_type}:{event_id}/summary.xml"
         res = await self.session.get(url)
@@ -378,6 +507,10 @@ class BetRadarData(Data):
                 n -= 1
 
     async def get_event_info(self, urn_type: str, event_id: int, n: int = 3):
+        """
+        sr:match
+        sr:stage
+        """
         topic = "BetRadar/event_info"
         url = f"{self.url}/sports/en/sport_events/sr:{urn_type}:{event_id}/timeline.xml"
         res = await self.session.get(url)
@@ -453,6 +586,23 @@ class BetRadarData(Data):
                 n -= 1
 
     async def get_info(self, urn_type: str, tournament_id: int, n: int = 3):
+        """
+        sr:tournament
+        sr:season
+        sr:stage
+        sr:simple_tournament
+        vf:tournament
+        vf:season
+        vbl:tournament
+        vbl:season
+        vto:tournament
+        vto:season
+        vdr:stage
+        vhc:stage
+        vti:tournament
+        vci:tournament
+        vbi:tournament
+        """
         topic = "BetRadar/info"
         url = f"{self.url}/sports/en/tournaments/sr:{urn_type}:{tournament_id}/info.xml"
         res = await self.session.get(url)
@@ -468,6 +618,11 @@ class BetRadarData(Data):
                 n -= 1
 
     async def get_all_seasons(self, urn_type: str, tournament_id: int, n: int = 3):
+        """
+        urn_type:
+        season
+        tournaments
+        """
         topic = "BetRadar/seasons"
         url = f"{self.url}/sports/en/tournaments/sr:{urn_type}:{tournament_id}/seasons.xml"
         res = await self.session.get(url)
@@ -481,6 +636,8 @@ class BetRadarData(Data):
             if not success and n > 0:
                 # success = await self._retry(topic=topic, obj=Runner, url=url)
                 n -= 1
+
+    ####################################################                    ###################################################################
 
     async def get_sport_fixture(self, urn_type: str, event_id: int, n: int = 3):
         topic = "BetRadar/sport_fixture"
