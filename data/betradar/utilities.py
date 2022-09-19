970fbebb59b93069217215952eeda6799c0a186e
DrunkRandomWalker <peiyun@injectiveprotocol.com>
2022-09-19 13:12:52 -0400

    clean up unused files

--- data/betradar/utilities.py
+++ data/betradar/utilities.py
@@ -5,14 +5,10 @@ import logging
 from aiohttp import ClientTimeout, TCPConnector, ClientSession
 from asyncio import sleep
 
-# from orjson import dumps
 from pickle import dumps
 
 from utils.utilities import RedisProducer
 
-# from utils.granter import Granter
-# from utils.markets import ActiveMarket, StagingMarket
-# from utils.client import create_client, switch_node_recreate_client
 from data.betradar.utilities import *
 
 
