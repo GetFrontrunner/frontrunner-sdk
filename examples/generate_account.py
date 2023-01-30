181d29e42dac6a844e161da7f5b1576f8ae6ae3b
DrunkRandomWalker <peiyun@injectiveprotocol.com>
2023-01-30 16:53:46 -0500

    wip

--- examples/generate_account.py
+++ examples/generate_account.py
@@ -20,7 +20,7 @@ env_name_1 = "INJ_ADDRESS"
 env_name_2 = "INJ_PRIVATE_KEY"
 
 
-def generate_mnemonic()->Dict[str, str]:
+def generate_mnemonic() -> Dict[str, str]:
     mnemonic, privkey = PrivateKey.generate()
     inj_address = privkey.to_public_key().to_address().to_acc_bech32()
     secret_obj = {
@@ -33,52 +33,47 @@ def generate_mnemonic()->Dict[str, str]:
     return secret_obj
 
 
-def get_eth_address_from_inj(inj_wallet_address:str)->str:
+def get_eth_address_from_inj(inj_wallet_address: str) -> str:
     address = Address.from_acc_bech32(inj_wallet_address)
     eth_address = address.get_ethereum_address()
     print(f"Got '{eth_address=}' from '{inj_wallet_address=}'")
     return eth_address
 
 
-def check_env_is_set(env_name:str):
+def check_env_is_set(env_name: str):
     try:
         if os.environ[env_name]:
-            print(f"{env_name}={os.environ[env_name]}")
+            print(f"error: {env_name} is set to {os.environ[env_name]}")
+            sys.exit(1)
     except KeyError:
-        print(f"{env_name} is not set.")
-        sys.exit(1)
+        pass
+        # print(f"{env_name} is not set.")
 
 
-def check_env_is_on(env_name:str):
+def check_env_is_on(env_name: str):
     # Checking the value of the environment variable
-    if os.environ.get(env_name) == "True":
-        print(f"{env_name} is on")
+    if os.environ.get(env_name):
+        print(f"{env_name}    is on")
     else:
-        print(f"{env_name} is off")
-        sys.exit(1)
+        print(f"{env_name}    is off")
+        # sys.exit(1)
 
 
-def set_env(env_name:str, env_value:str):
+def set_env(env_name: str, env_value: str):
     os.environ.setdefault(env_name, env_value)
-    print(f"{env_name} is set")
+    os.system(f"bash -c 'echo \"export {env_name}={env_value}\" >> .env'")
+    print(f"{env_name} has saved to .env")
 
 
-#def check_env_variables():
-#    #    1. `export INJ_ADDRESS=<inj_address_from_above>`
-#    #    2. `export INJ_PRIVATE_KEY=<inj_private_from_above>`
-#    for env_name in (env_name_1, env_name_2):
-#        check_env_is_set(env_name)
-
-
-def set_env_variables(secret_obj:Dict[str, str]):
+def set_env_variables(secret_obj: Dict[str, str]):
     for key, value in secret_obj.items():
         if key in ("inj_address", "inj_private_key"):
             check_env_is_set(key.upper())
             set_env(key.upper(), value)
-            check_env_is_on(key.upper())
 
 
-def request_test_tokens(inj_address:str):
+def request_test_tokens(inj_address: str):
+    print("requesting test tokens(this may take several seconds)")
     response = requests.post(
         f"https://knroo5qf2e.execute-api.us-east-2.amazonaws.com/default/TestnetFaucetAPI?address={inj_address}"
     )
@@ -87,15 +82,22 @@ def request_test_tokens(inj_address:str):
         print("please request your test tokens from: https://testnet.faucet.injective.network")
     else:
         print("succeeded in requesting test tokens")
-        sys.exit(1)
 
 
 def main():
     secret_obj = generate_mnemonic()
+
     set_env_variables(secret_obj)
+
+    for key in ("inj_address", "inj_private_key"):
+        check_env_is_on(key.upper())
+
+    os.system("bash -c 'cat .env'")
+    os.system("bash -c 'source .env'")
+
     request_test_tokens(secret_obj["inj_address"])
+    print("run 'source .env' to set env variables")
 
 
 if __name__ == "__main__":
     main()
-
