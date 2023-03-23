9df5c76c26a308ee47519ced7950aeb589e8e4fa
Brian To <brian.to@getfrontrunner.com>
2023-03-23 12:17:07 -0400

    clean up test code

--- tests/commands/injective/test_create_wallet.py
+++ tests/commands/injective/test_create_wallet.py
@@ -1,18 +1,15 @@
 from unittest import IsolatedAsyncioTestCase
+from unittest.mock import MagicMock
 
 from frontrunner_sdk.commands.injective.create_wallet import CreateWalletOperation  # NOQA
 from frontrunner_sdk.commands.injective.create_wallet import CreateWalletRequest  # NOQA
 from frontrunner_sdk.ioc import FrontrunnerIoC
 
 
-class TestIoC(FrontrunnerIoC):
-  pass
-
-
 class TestCreateWalletOperation(IsolatedAsyncioTestCase):
 
   def setUp(self) -> None:
-    self.deps = TestIoC()
+    self.deps = MagicMock(spec=FrontrunnerIoC)
 
   def test_validate_pass(self):
     req = CreateWalletRequest()
--- tests/facades/test_injective.py
+++ tests/facades/test_injective.py
@@ -1,18 +1,15 @@
 from unittest import IsolatedAsyncioTestCase
+from unittest.mock import MagicMock
 
 from frontrunner_sdk.facades.injective import InjectiveAsync
 from frontrunner_sdk.ioc import FrontrunnerIoC
 
 
-class TestIoC(FrontrunnerIoC):
-  pass
-
-
 class TestInjectiveAsync(IsolatedAsyncioTestCase):
 
   def setUp(self) -> None:
-    self.ioc = TestIoC()
-    self.injective = InjectiveAsync(self.ioc)
+    self.deps = MagicMock(spec=FrontrunnerIoC)
+    self.injective = InjectiveAsync(self.deps)
 
   async def test_create_wallet(self):
     response = await self.injective.create_wallet()
