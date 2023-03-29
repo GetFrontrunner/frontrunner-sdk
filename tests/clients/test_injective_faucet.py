3c56a9186e157fc489bcc6a8e1a1ddea88cc7ee0
Brian To <brian.to@getfrontrunner.com>
2023-03-29 18:54:48 -0400

    make fund wallet take in wallet instead of address

--- tests/clients/test_injective_faucet.py
+++ tests/clients/test_injective_faucet.py
@@ -10,11 +10,13 @@ from frontrunner_sdk.clients.injective_faucet import InjectiveFaucet
 from frontrunner_sdk.exceptions import FrontrunnerConfigurationException
 from frontrunner_sdk.exceptions import FrontrunnerInjectiveException
 from frontrunner_sdk.exceptions import FrontrunnerUnserviceableException
+from frontrunner_sdk.models.wallet import Wallet
 
 
 class TestInjectiveFaucet(AioHTTPTestCase):
 
   def setUp(self):
+    self.wallet = Wallet._new()
     self.fund_wallet_response = web.Response()
 
   async def setUpAsync(self) -> None:
@@ -43,24 +45,24 @@ class TestInjectiveFaucet(AioHTTPTestCase):
   async def test_fund_wallet_success(self):
     self.fund_wallet_response = web.Response(text=json.dumps({"message": "Works"}))
 
-    res = await self.injective_faucet.fund_wallet(address="<fake>")
+    res = await self.injective_faucet.fund_wallet(self.wallet)
     self.assertEquals(res, {"message": "Works"})
 
   async def test_fund_wallet_failure(self):
     self.fund_wallet_response = web.Response(status=400, text=json.dumps({"message": "Bad Request"}))
 
     with self.assertRaises(FrontrunnerInjectiveException):
-      await self.injective_faucet.fund_wallet(address="<fake>")
+      await self.injective_faucet.fund_wallet(self.wallet)
 
   async def test_fund_wallet_unserviceable_server_error(self):
     self.fund_wallet_response = web.Response(status=500, text=json.dumps({"message": "Internal Server Error"}))
 
     with self.assertRaises(FrontrunnerUnserviceableException):
-      await self.injective_faucet.fund_wallet(address="<fake>")
+      await self.injective_faucet.fund_wallet(self.wallet)
 
   async def test_fund_wallet_unserviceable_infrastructure(self):
     with patch.object(ClientSession, "post") as _post:
       _post.side_effect = ClientError("Bad URL")
 
       with self.assertRaises(FrontrunnerUnserviceableException):
-        await self.injective_faucet.fund_wallet(address="<fake>")
+        await self.injective_faucet.fund_wallet(self.wallet)
