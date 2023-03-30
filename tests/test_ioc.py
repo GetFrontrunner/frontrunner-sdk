6ff8b31b4f16e9770bb18ff0d6858f9ddbeaddd5
Brian To <brian.to@getfrontrunner.com>
2023-03-29 22:15:04 -0400

    update tests for ioc

--- tests/test_ioc.py
+++ tests/test_ioc.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 
 from frontrunner_sdk.clients.injective_chain import InjectiveChain
+from frontrunner_sdk.clients.injective_faucet import InjectiveFaucet
+from frontrunner_sdk.clients.injective_light_client_daemon import InjectiveLightClientDaemon
 from frontrunner_sdk.config.base import FrontrunnerConfig
 from frontrunner_sdk.ioc import FrontrunnerIoC
 
@@ -15,3 +17,9 @@ class TestFrontrunnerIoC(TestCase):
 
   def test_injective_chain(self):
     self.assertIsInstance(self.ioc.injective_chain, InjectiveChain)
+
+  def test_injective_faucet(self):
+    self.assertIsInstance(self.ioc.injective_faucet, InjectiveFaucet)
+
+  def test_injective_light_client_daemon(self):
+    self.assertIsInstance(self.ioc.injective_light_client_daemon, InjectiveLightClientDaemon)
