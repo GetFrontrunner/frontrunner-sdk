9bb12e3ca74eee9610cb1deb6538b511cd855025
Brian To <brian.to@getfrontrunner.com>
2023-03-30 00:57:14 -0400

    add injective lcd client tests

--- frontrunner_sdk/clients/injective_light_client_daemon.py
+++ frontrunner_sdk/clients/injective_light_client_daemon.py
@@ -2,6 +2,8 @@ import logging
 from typing import Optional
 
 from frontrunner_sdk.exceptions import FrontrunnerConfigurationException
+from frontrunner_sdk.exceptions import FrontrunnerInjectiveException
+from frontrunner_sdk.exceptions import FrontrunnerUnserviceableException
 from frontrunner_sdk.logging.log_external_exceptions import log_external_exceptions  # NOQA
 from frontrunner_sdk.models.wallet import Wallet
 
@@ -20,7 +22,25 @@ class InjectiveLightClientDaemon:
   async def initialize_wallet(self, wallet: Wallet) -> None:
     logger.debug("Calling Injective LCD to initialize wallet with wallet=%s", wallet.injective_address)
 
-    await wallet.address.async_init_num_seq(self.base_url)
+    try:
+      await wallet.address.async_init_num_seq(self.base_url)
+
+    except ValueError as cause:
+      _, status = cause.args
+
+      if status >= 500:
+        raise FrontrunnerUnserviceableException(
+          "Injective light client daemon (lcd) endpoint unavailable",
+          base_url=self.base_url,
+          address=wallet.injective_address,
+        ) from cause
+
+      else:
+        raise FrontrunnerInjectiveException(
+          "Could not initialize wallet",
+          base_url=self.base_url,
+          address=wallet.injective_address,
+        ) from cause
 
     logger.debug(
       "Received response from Injective LCD yielding wallet=%s account_number=%d sequence=%d",
