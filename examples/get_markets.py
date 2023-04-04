f748734702913e3267a6c7a62b7effb32eda5a17
DrunkRandomWalker <peiyun@injectiveprotocol.com>
2023-04-04 11:15:07 -0400

    wip

--- examples/get_markets.py
+++ examples/get_markets.py
@@ -11,25 +11,37 @@ from swagger_client import Configuration, MarketStatus
 
 def parse_cli_arguments() -> Namespace:
   parser = argparse.ArgumentParser()
+
   parser.add_argument("--id", help="frontrunner market id")
   parser.add_argument("--inj_id", help="injective market id")
   parser.add_argument("--prop_id", help="frontrunner prop id")
   parser.add_argument("--event_id", help="frontrunner sport event id")
   parser.add_argument("--league_id", help="frontrunner league id")
+  parser.add_argument("--status", help="frontrunner market status",choices=['active', 'closed'], default='active')
   args = parser.parse_args()
   return args
 
+def valid_args(args):
+  if (args['status']=='closed' and len(args)==1):
+    raise Exception("Need to provide at least one of id, inj_id, prop_id, event_id, league_id when market status is closed")
 
-async def run_get_markets(namespace: Namespace, configuration: Configuration, status: Optional[MarketStatus]):
+async def run_get_markets(namespace: Namespace, configuration: Configuration):
   api_instance = swagger_client.FrontrunnerApi(swagger_client.ApiClient(configuration))
 
   kwargs = {}
+
   for arg in vars(namespace):
     if getattr(namespace, arg):
-      kwargs[arg] = getattr(namespace, arg)
+      if arg == 'status':
+        if getattr(namespace, arg) == 'active':
+          kwargs[arg] = swagger_client.MarketStatus.ACTIVE
+        else:
+          kwargs[arg] = swagger_client.MarketStatus.CLOSED
+      else:
+        kwargs[arg] = getattr(namespace, arg)
 
-  if status:
-    kwargs['status'] = status
+  valid_args(kwargs)
+
   try:
     api_response = api_instance.get_markets(**kwargs)
     pprint(api_response)
@@ -42,7 +54,13 @@ async def main():
   configuration = swagger_client.Configuration()
   configuration.api_key['Authorization'] = frontrunner_api_key
   namespace = parse_cli_arguments()
-  await run_get_markets(namespace, configuration, None)
+  if getattr(namespace, 'status')=="active":
+    pass
+  else:
+    pass
+
+    
+  await run_get_markets(namespace, configuration)
 
 
 if __name__ == "__main__":
