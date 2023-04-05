f4ec74f55d57869c2fe9f213006503c2c2cdea10
Brian To <126547568+brianto-frontrunner@users.noreply.github.com>
2023-04-05 01:28:29 -0400

    feat: add frontrunner list markets command (#31)

--- frontrunner_sdk/sdk.py
+++ frontrunner_sdk/sdk.py
@@ -1,5 +1,7 @@
 from functools import cached_property
 
+from frontrunner_sdk.facades.frontrunner import Frontrunner
+from frontrunner_sdk.facades.frontrunner import FrontrunnerAsync
 from frontrunner_sdk.facades.injective import Injective
 from frontrunner_sdk.facades.injective import InjectiveAsync
 from frontrunner_sdk.ioc import FrontrunnerIoC
@@ -12,6 +14,10 @@ class FrontrunnerSDKAsync:
     return FrontrunnerIoC()
 
   @cached_property
+  def frontrunner(self) -> FrontrunnerAsync:
+    return FrontrunnerAsync(self.dependencies)
+
+  @cached_property
   def injective(self) -> InjectiveAsync:
     return InjectiveAsync(self.dependencies)
 
@@ -21,6 +27,10 @@ class FrontrunnerSDK:
   @cached_property
   def dependencies(self):
     return FrontrunnerIoC()
+
+  @cached_property
+  def frontrunner(self) -> Frontrunner:
+    return Frontrunner(self.dependencies)
 
   @cached_property
   def injective(self) -> Injective:
