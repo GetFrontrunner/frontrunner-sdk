201094ccbadad7a26e5557c8e59a6f9ef8f9c2f0
brianvs <brian@getfrontrunner.com>
2023-04-06 14:43:38 -0400

    add unit test for operation

--- tests/commands/injective/test_load_wallet.py
+++ tests/commands/injective/test_load_wallet.py
@@ -2,7 +2,8 @@ from unittest import IsolatedAsyncioTestCase
 from unittest.mock import AsyncMock
 from unittest.mock import MagicMock
 
-from frontrunner_sdk.commands.injective.load_wallet import LoadWalletFromMnemonicOperation # NOQA
+from frontrunner_sdk.commands.injective.load_wallet import LoadWalletFromMnemonicOperation, LoadWalletFromPrivateKeyRequest, \
+  LoadWalletFromPrivateKeyOperation  # NOQA
 from frontrunner_sdk.commands.injective.load_wallet import LoadWalletFromMnemonicRequest # NOQA
 from frontrunner_sdk.commands.injective.load_wallet import LoadWalletFromMnemonicResponse # NOQA
 from frontrunner_sdk.ioc import FrontrunnerIoC
@@ -29,6 +30,30 @@ class TestLoadWalletFromMnemonicOperation(IsolatedAsyncioTestCase):
     res = await cmd.execute(self.deps)
 
     self.assertEqual(res.wallet.mnemonic, self.wallet.mnemonic)
+    self.assertEqual(res.wallet.private_key.to_hex(), self.wallet.private_key.to_hex())
+
+    self.deps.injective_light_client_daemon.initialize_wallet.assert_awaited_once()
+
+
+class TestLoadWalletFromPrivateKeyOperation(IsolatedAsyncioTestCase):
+
+  def setUp(self) -> None:
+    self.wallet = Wallet._new()
+    self.private_key = self.wallet.private_key
+    self.deps = MagicMock(spec=FrontrunnerIoC)
+
+  def test_validate(self):
+    req = LoadWalletFromPrivateKeyRequest(private_key=self.private_key)
+    cmd = LoadWalletFromPrivateKeyOperation(req)
+    cmd.validate(self.deps)
+
+  async def test_load_wallet_from_private_key(self):
+    self.deps.injective_light_client_daemon.initialize_wallet = AsyncMock()
+
+    req = LoadWalletFromPrivateKeyRequest(private_key=self.private_key.to_hex())
+    cmd = LoadWalletFromPrivateKeyOperation(req)
+    res = await cmd.execute(self.deps)
+
     self.assertEqual(res.wallet.private_key.to_hex(), self.wallet.private_key.to_hex())
 
     self.deps.injective_light_client_daemon.initialize_wallet.assert_awaited_once()
