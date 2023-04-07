ef19ebef5a2c9ff3639c479d314cb3094c5f2be2
Brian To <brian.to@getfrontrunner.com>
2023-04-07 10:10:49 -0400

    add failure test case

--- tests/test_ioc.py
+++ tests/test_ioc.py
@@ -4,6 +4,7 @@ from frontrunner_sdk.clients.injective_chain import InjectiveChain
 from frontrunner_sdk.clients.injective_faucet import InjectiveFaucet
 from frontrunner_sdk.clients.injective_light_client_daemon import InjectiveLightClientDaemon # NOQA
 from frontrunner_sdk.config.base import FrontrunnerConfig
+from frontrunner_sdk.exceptions import FrontrunnerConfigurationException
 from frontrunner_sdk.ioc import FrontrunnerIoC
 from frontrunner_sdk.openapi.frontrunner_api import FrontrunnerApi
 
@@ -15,6 +16,10 @@ class TestFrontrunnerIoC(TestCase):
 
   def test_config(self):
     self.assertIsInstance(self.ioc.config, FrontrunnerConfig)
+
+  def test_wallet_not_configured(self):
+    with self.assertRaises(FrontrunnerConfigurationException):
+      self.ioc.wallet
 
   def test_openapi_frontrunner_api(self):
     self.assertIsInstance(self.ioc.openapi_frontrunner_api, FrontrunnerApi)
