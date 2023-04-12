1c7ea4a4fee6463cd16af8bbdaeffb353dccc405
Brian To <126547568+brianto-frontrunner@users.noreply.github.com>
2023-04-12 12:25:39 -0400

    feat: add list positions (#41)
    # Testing
    
    ```
    import logging
    from frontrunner_sdk import FrontrunnerSDK
    logging.getLogger("frontrunner_sdk").setLevel(logging.WARNING)
    sdk = FrontrunnerSDK()
    
    markets = sdk.frontrunner.find_markets(
      sports=["formula1"],
      sport_entity_abbreviations=["VER"],
    )
    
    positions = sdk.injective.get_positions(markets.market_ids)
    ```
    
    ![Screenshot 2023-04-12 at 12 20
    49](https://user-images.githubusercontent.com/126547568/231520323-0aaf4e11-0590-4ed6-86a5-74b5ecd8b2a5.png)

---
+++ tests/helpers/test_paginators.py
@@ -0,0 +1,75 @@
+from typing import Any
+from typing import Callable
+from typing import Iterable
+from typing import Sized
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import AsyncMock
+from unittest.mock import MagicMock
+
+from frontrunner_sdk.helpers.paginators import injective_paginated_iterator
+from frontrunner_sdk.helpers.paginators import injective_paginated_list
+
+
+class TestInjectivePaginator(IsolatedAsyncioTestCase):
+
+  @staticmethod
+  def _paged_call(field: str, pages: Iterable[Sized]) -> Callable[..., Any]:
+    total = sum(len(page) for page in pages)
+
+    return AsyncMock(side_effect=[MagicMock(
+      paging=MagicMock(total=total),
+      **{field: page},
+    ) for page in pages])
+
+  async def test_iterator_one_page(self):
+    iterator = injective_paginated_iterator(
+      self._paged_call("items", [
+        [1, 2, 3],
+      ]),
+      "items",
+    )
+
+    self.assertEqual(
+      [item async for item in iterator],
+      [1, 2, 3],
+    )
+
+  async def test_iterator_multiple_pages(self):
+    iterator = injective_paginated_iterator(
+      self._paged_call("numbers", [
+        [1, 2, 3],
+        [4, 5],
+      ]),
+      "numbers",
+    )
+
+    self.assertEqual(
+      [item async for item in iterator],
+      [1, 2, 3, 4, 5],
+    )
+
+  async def test_iterator_empty_pages(self):
+    iterator = injective_paginated_iterator(
+      self._paged_call("numbers", [
+        [1, 2],
+        list(),
+        [3, 4, 5],
+      ]),
+      "numbers",
+    )
+
+    self.assertEqual(
+      [item async for item in iterator],
+      [1, 2, 3, 4, 5],
+    )
+
+  async def test_paginated_list(self):
+    result = await injective_paginated_list(
+      self._paged_call("numbers", [
+        ["my", "name"],
+        ["is", "earl"],
+      ]),
+      "numbers",
+    )
+
+    self.assertEqual(result, ["my", "name", "is", "earl"])
