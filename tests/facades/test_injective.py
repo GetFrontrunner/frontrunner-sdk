c9a87b96f9a63dc7bebd5de4af746fbc74d6410d
Brian To <126547568+brianto-frontrunner@users.noreply.github.com>
2023-04-12 15:45:32 -0400

    feat: add get my orders (#43)
    # Testing
    
    ```
    In [1]: import logging
       ...: from frontrunner_sdk import FrontrunnerSDK
       ...: logging.getLogger("frontrunner_sdk").setLevel(logging.WARNING)
       ...: sdk = FrontrunnerSDK()
    
    In [2]: orders = sdk.injective.get_my_orders()
    INFO:chain session cookie loaded from disk:
    
    In [3]: orders.orders
    Out[3]: []
    ```

--- tests/facades/test_injective.py
+++ tests/facades/test_injective.py
@@ -13,6 +13,8 @@ from frontrunner_sdk.commands.injective.fund_wallet_from_faucet import FundWalle
 from frontrunner_sdk.commands.injective.fund_wallet_from_faucet import FundWalletFromFaucetResponse # NOQA
 from frontrunner_sdk.commands.injective.get_account_portfolio import GetAccountPortfolioOperation # NOQA
 from frontrunner_sdk.commands.injective.get_account_portfolio import GetAccountPortfolioResponse # NOQA
+from frontrunner_sdk.commands.injective.get_my_orders import GetMyOrdersOperation # NOQA
+from frontrunner_sdk.commands.injective.get_my_orders import GetMyOrdersResponse # NOQA
 from frontrunner_sdk.commands.injective.get_order_books import GetOrderBooksOperation # NOQA
 from frontrunner_sdk.commands.injective.get_order_books import GetOrderBooksResponse # NOQA
 from frontrunner_sdk.commands.injective.get_positions import GetPositionsOperation # NOQA
@@ -89,6 +91,16 @@ class TestInjectiveFacadeAsync(IsolatedAsyncioTestCase):
   )
   async def test_get_order_books(self, _execute: AsyncMock):
     await self.facade.get_order_books(market_ids=["abc"])
+    _execute.assert_awaited_once()
+
+  @patch.object(
+    GetMyOrdersOperation,
+    "execute",
+    new_callable=AsyncMock,
+    return_value=GetMyOrdersResponse(orders=MagicMock()),
+  )
+  async def test_get_my_orders(self, _execute: AsyncMock):
+    await self.facade.get_my_orders()
     _execute.assert_awaited_once()
 
   @patch.object(
