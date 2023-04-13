b0ab530ccaad82f2e3670467e8d9f317623fa78b
Brian To <brian.to@getfrontrunner.com>
2023-04-13 13:04:25 -0400

    fix: add missing skip= kwarg to paginated calls

--- tests/helpers/test_paginators.py
+++ tests/helpers/test_paginators.py
@@ -1,9 +1,8 @@
-from typing import Any
-from typing import Callable
 from typing import Iterable
 from typing import Sized
 from unittest import IsolatedAsyncioTestCase
 from unittest.mock import AsyncMock
+from unittest.mock import call
 from unittest.mock import MagicMock
 
 from frontrunner_sdk.helpers.paginators import injective_paginated_iterator
@@ -13,7 +12,7 @@ from frontrunner_sdk.helpers.paginators import injective_paginated_list
 class TestInjectivePaginator(IsolatedAsyncioTestCase):
 
   @staticmethod
-  def _paged_call(field: str, pages: Iterable[Sized]) -> Callable[..., Any]:
+  def _paged_call(field: str, pages: Iterable[Sized]) -> AsyncMock:
     total = sum(len(page) for page in pages)
 
     return AsyncMock(side_effect=[MagicMock(
@@ -35,18 +34,24 @@ class TestInjectivePaginator(IsolatedAsyncioTestCase):
     )
 
   async def test_iterator_multiple_pages(self):
-    iterator = injective_paginated_iterator(
-      self._paged_call("numbers", [
-        [1, 2, 3],
-        [4, 5],
-      ]),
-      "numbers",
-    )
+    paged_call = self._paged_call("numbers", [
+      [1, 2, 3],
+      [4, 5],
+      [6, 7, 8, 9],
+    ])
+
+    iterator = injective_paginated_iterator(paged_call, "numbers")
 
     self.assertEqual(
       [item async for item in iterator],
-      [1, 2, 3, 4, 5],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
     )
+
+    paged_call.assert_has_awaits([
+      call(),
+      call(skip=3),
+      call(skip=5),
+    ])
 
   async def test_iterator_empty_pages(self):
     iterator = injective_paginated_iterator(
