4966888e4c9b737b4073c76e7df9c435cb120c79
Brian Vander Schaaf <128427624+bvs-frontrunner@users.noreply.github.com>
2023-04-18 12:18:33 -0400

    feat: stream trades (#47)
    ```python
    import asyncio
    
    from frontrunner_sdk import FrontrunnerSDKAsync
    
    async def main_async():
        sdk = FrontrunnerSDKAsync()
        response = await sdk.injective.stream_trades(["0xc0965744324e4a4161fff8678e78bacf3f51e46383086e869ce089ae27277562"])
        async for trade in response.trades:
            print(trade)
    
    def main():
        pass
    
    if __name__ == "__main__":
        asyncio.run(main_async())
    ```
    after submitting a market order via Frontrunner:
    ```
    INFO:chain session cookie loaded from disk:
    INFO:StreamTradesOperation with StreamTradesRequest(market_ids=['0xc0965744324e4a4161fff8678e78bacf3f51e46383086e869ce089ae27277562'], mine=False, direction=None, side=None, start_time=None, end_time=None), yielding StreamTradesResponse(trades=<async_generator object injective_stream_iterator at 0x110102430>)
    order_hash: "0x910a8994fb8a02547b2f0378eaf3a5e276c42df93ebc7de4b223dd1a0cd25377"
    subaccount_id: "0xb4efdbe3240d3d2a1bc6be8a1f717944e734a0dd000000000000000000000001"
    market_id: "0xc0965744324e4a4161fff8678e78bacf3f51e46383086e869ce089ae27277562"
    trade_execution_type: "limitFill"
    position_delta {
      trade_direction: "sell"
      execution_price: "210000"
      execution_quantity: "23"
      execution_margin: "18170000"
    }
    payout: "0"
    fee: "0"
    executed_at: 1681832508278
    fee_recipient: "inj1knhahceyp57j5x7xh69p7utegnnnfgxavmahjr"
    trade_id: "10455394_19_0"
    execution_side: "maker"
    
    order_hash: "0xdf5c94f7ff69aa22656151216c787005f1d0bcaa052d7d5b3eaf9a052afb8f68"
    subaccount_id: "0x4cbe99129d6ce15dc240c5e2c04af986dd29f0b9000000000000000000000000"
    market_id: "0xc0965744324e4a4161fff8678e78bacf3f51e46383086e869ce089ae27277562"
    trade_execution_type: "market"
    position_delta {
      trade_direction: "buy"
      execution_price: "210000"
      execution_quantity: "23"
      execution_margin: "4830000"
    }
    payout: "0"
    fee: "0"
    executed_at: 1681832508278
    fee_recipient: "inj1fjlfjy5adns4msjqch3vqjhesmwjnu9ep045wz"
    trade_id: "10455394_18_0"
    execution_side: "taker"
    ```

---
+++ frontrunner_sdk/helpers/streams.py
@@ -0,0 +1,31 @@
+from typing import Any
+from typing import AsyncIterable
+from typing import AsyncIterator
+from typing import Awaitable
+from typing import Callable
+from typing import TypeVar
+
+Item = TypeVar("Item")
+
+
+async def injective_stream_iterator(
+  call: Callable[..., Awaitable[AsyncIterable]],
+  field: str,
+  *call_args: Any,
+  **call_kwargs: Any,
+) -> AsyncIterator[Item]:
+
+  response = await call(*call_args, **call_kwargs)
+
+  async for item in response:
+    yield getattr(item, field)
+
+
+async def injective_stream(
+  call: Callable[..., Awaitable[AsyncIterable]],
+  field: str,
+  *call_args: Any,
+  **call_kwargs: Any,
+) -> AsyncIterator[Item]:
+  response: AsyncIterator[Item] = injective_stream_iterator(call, field, *call_args, **call_kwargs)
+  return response
