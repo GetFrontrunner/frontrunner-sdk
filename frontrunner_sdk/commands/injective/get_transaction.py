e8e2d95fb927fe1c0b4f42f153bb61bdd00747fb
brianvs <brian@getfrontrunner.com>
2023-08-10 15:21:55 -0400

    enhance to include order failures

--- frontrunner_sdk/commands/injective/get_transaction.py
+++ frontrunner_sdk/commands/injective/get_transaction.py
@@ -1,10 +1,37 @@
+import json
 from dataclasses import dataclass
+from typing import Optional, List
 
 from pyinjective.proto.cosmos.tx.v1beta1.service_pb2 import GetTxResponse
 
 from frontrunner_sdk.commands.base import FrontrunnerOperation
 from frontrunner_sdk.ioc import FrontrunnerIoC
 from frontrunner_sdk.logging.log_operation import log_operation
+
+@dataclass
+class OrderFailure:
+  flags: List[int]
+  hashes: List[str]
+
+  EVENT_ORDER_FAIL_TYPE = "injective.exchange.v1beta1.EventOrderFail"
+
+  @classmethod
+  def from_injective_response(clz, injective_response: GetTxResponse) -> list:
+    result = []
+    if getattr(injective_response, "tx_response"):
+      tx_response = injective_response.tx_response
+      if getattr(tx_response, "logs"):
+        logs = tx_response.logs
+        if logs:
+          for log in logs:
+            for event in log.events:
+              if event.type == clz.EVENT_ORDER_FAIL_TYPE:
+                attributes = event.attributes
+                flags: List[int] = next((json.loads(attribute.value) for attribute in attributes if attribute.key == "flags"), [])
+                hashes: List[str] = next((json.loads(attribute.value) for attribute in attributes if attribute.key == "hashes"), [])
+                result.append(clz(flags, hashes))
+
+    return result
 
 
 @dataclass
@@ -14,7 +41,8 @@ class GetTransactionRequest:
 
 @dataclass
 class GetTransactionResponse:
-  response: GetTxResponse
+  injective_response: GetTxResponse
+  order_failures: Optional[List[OrderFailure]] = None
 
 
 class GetTransactionOperation(FrontrunnerOperation[GetTransactionRequest, GetTransactionResponse]):
@@ -27,8 +55,6 @@ class GetTransactionOperation(FrontrunnerOperation[GetTransactionRequest, GetTra
 
   @log_operation(__name__)
   async def execute(self, deps: FrontrunnerIoC) -> GetTransactionResponse:
-    # for now, this is a straight proxy call, and we return the raw result;
-    # return may be augmented in the future.
     response = await deps.injective_client.get_tx(self.request.transaction_hash)
-
-    return GetTransactionResponse(response=response)
+    order_failures = OrderFailure.from_injective_response(response)
+    return GetTransactionResponse(injective_response=response, order_failures=order_failures)
