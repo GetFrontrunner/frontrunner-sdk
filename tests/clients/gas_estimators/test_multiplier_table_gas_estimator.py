abe2239902a37319ffb594ab9e67e171423a3769
Brian Vander Schaaf <128427624+bvs-frontrunner@users.noreply.github.com>
2023-08-24 10:40:31 -0400

    feat: add new MultiplierTableGasEstimator for increased gas estimation (#107)
    This multiplies all per-message gas estimates by 2 (and keeps the
    additional 80k offset on top of the per-message gas estimates).

---
+++ tests/clients/gas_estimators/test_multiplier_table_gas_estimator.py
@@ -0,0 +1,52 @@
+from unittest import IsolatedAsyncioTestCase
+
+from pyinjective.proto.injective.exchange.v1beta1.exchange_pb2 import DerivativeOrder # NOQA
+from pyinjective.proto.injective.exchange.v1beta1.tx_pb2 import MsgBatchUpdateOrders # NOQA
+from pyinjective.proto.injective.exchange.v1beta1.tx_pb2 import MsgCreateSpotMarketOrder # NOQA
+from pyinjective.proto.injective.exchange.v1beta1.tx_pb2 import MsgDeposit
+from pyinjective.proto.injective.exchange.v1beta1.tx_pb2 import MsgRewardsOptOut # NOQA
+from pyinjective.proto.injective.exchange.v1beta1.tx_pb2 import OrderData
+
+from frontrunner_sdk.clients.gas_estimators.multiplier_table_gas_estimator import MultiplierTableGasEstimator # NOQA
+from frontrunner_sdk.clients.gas_estimators.table_gas_estimator import TableGasEstimator # NOQA
+from frontrunner_sdk.exceptions import FrontrunnerInjectiveException
+
+
+class TestOffsettingTableGasEstimator(IsolatedAsyncioTestCase):
+
+  def setUp(self):
+    self.estimator = MultiplierTableGasEstimator(2)
+
+  async def test_gas_for_unknown_message(self):
+    msg = MsgRewardsOptOut()
+
+    with self.assertRaises(FrontrunnerInjectiveException):
+      await self.estimator.gas_for(msg)
+
+  async def test_gas_for_unknown_order(self):
+    msg = MsgCreateSpotMarketOrder()
+
+    with self.assertRaises(FrontrunnerInjectiveException):
+      await self.estimator.gas_for(msg)
+
+  async def test_gas_for_non_composite_message(self):
+    msg = MsgDeposit()
+
+    self.assertEqual(
+      self.estimator.multiplier * TableGasEstimator.MESSAGE_RATES["MsgDeposit"],
+      await self.estimator.gas_for(msg),
+    )
+
+  async def test_gas_for_composite_message(self):
+    msg = MsgBatchUpdateOrders(
+      binary_options_orders_to_create=[DerivativeOrder(), DerivativeOrder()],
+      binary_options_orders_to_cancel=[OrderData()],
+    )
+
+    self.assertEqual(
+      self.estimator.multiplier * (
+        TableGasEstimator.MESSAGE_RATES["MsgBatchUpdateOrders"] + 2 * TableGasEstimator.ORDER_RATES["DerivativeOrder"] +
+        1 * TableGasEstimator.ORDER_RATES["OrderData"]
+      ),
+      await self.estimator.gas_for(msg),
+    )
